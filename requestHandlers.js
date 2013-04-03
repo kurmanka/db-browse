992d4bcc071ddbedb0f2ed5cac799001a96c321c
Mary Chistyakova <chistyakova.m@tut.by>
2013-04-03 11:24:58 +0300

    added ability to use spaces in the name of the group

--- requestHandlers.js
+++ requestHandlers.js
@@ -59,12 +59,12 @@ function getArrayOfStrings(tableGroups, done) {
     var i = 0;
 
     while (/\S/.exec(tableGroups)) {
-        var temp = /[^\n]+\n*/.exec(tableGroups);
+        var temp = /[^\n]+[\n|\r]*/.exec(tableGroups);
         if (/\S/.exec(temp)) {
-            array[i] = /\s*\S+/.exec(temp);
+            array[i] = temp.join().replace(/\n|\r/g, '');
             i++;
         }
-        tableGroups = tableGroups.replace(/[^\n]+\n*/, '');
+        tableGroups = tableGroups.replace(/[^\n]+[\n|\r]*/, '');
     }
 
     done(null, array);
