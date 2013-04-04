6c7c6787fbe54ba7a49d4223a5826aeb53124a4f
Mary Chistyakova <chistyakova.m@tut.by>
2013-04-04 12:14:19 +0300

    made caching (pool) the connections to the database

--- router.js
+++ router.js
@@ -1,11 +1,9 @@
 var async = require('async');
 var config = require("./config");
+var mysql = require('mysql');
+var pg = require('pg');
 
-var connectionStatus = {
-    baseId: '',
-    status: false,
-    connection: '',
-}
+var connectionStatus = {};
 
 function route(handle, pathname, response, request) {
     var checker = urlChecker(pathname);
@@ -51,39 +49,57 @@ function urlChecker(pathnameTemp) {
 }
 
 function dbConnect(dbId, handle, response, done) {
-    var mysql = require('mysql');
-    var pg = require('pg');
-
-    if (connectionStatus.baseId == dbId && connectionStatus.status == true) {
-        done(null);
-    } else {
-        console.log("Connect to database " + dbId + "..");
-
-        if (config.db[dbId] && config.db[dbId].type == 'mysql') {
-            connectionStatus.connection = mysql.createConnection({
-                host     : config.db[dbId].host,
-                user     : config.db[dbId].user,
-                password : config.db[dbId].password,
-                database : config.db[dbId].database,
-            });
-
-            connectionStatus.connection.connect(function(err) {
-                done(err);
-            });
+    if (!connectionStatus[dbId]) {
+        connectionStatus[dbId] = {
+            status: false,
+            connection: '',
         }
+    }
 
-        else if (config.db[dbId] && config.db[dbId].type == 'postgres') {
-            var conString = "tcp://postgres:" + config.db[dbId].password + "@" + config.db[dbId].host + "/" + config.db[dbId].database;
-            connectionStatus.connection = new pg.Client(conString);
+    if (connectionStatus[dbId].status == true) {
+        connectionStatus[dbId].connection.query("SELECT NOW();", function(err, rows, fields) {
+            if (err) {
+                connectionStatus[dbId].status = false;
+                makeConnect (dbId, handle, response, done);
+            } else {
+                done(null);
+            }
+        });
+        console.log('!! ' + connectionStatus[dbId].status);
+    }
 
-            connectionStatus.connection.connect(function(err) {
-                done(err);
-            });
-        }
+    if (connectionStatus[dbId].status == false) {
+        makeConnect (dbId, handle, response, done);
+    }
+}
 
-        else {
-            handle["showError"](response, "The database with id '" +  dbId + "' is absent in the configuration");
-        }
+function makeConnect (dbId, handle, response, done) {
+    console.log("Connect to database " + dbId + "..");
+
+    if (config.db[dbId] && config.db[dbId].type == 'mysql') {
+        connectionStatus[dbId].connection = mysql.createConnection({
+            host     : config.db[dbId].host,
+            user     : config.db[dbId].user,
+            password : config.db[dbId].password,
+            database : config.db[dbId].database,
+        });
+
+        connectionStatus[dbId].connection.connect(function(err) {
+            done(err);
+        });
+    }
+
+    else if (config.db[dbId] && config.db[dbId].type == 'postgres') {
+        var conString = "tcp://postgres:" + config.db[dbId].password + "@" + config.db[dbId].host + "/" + config.db[dbId].database;
+        connectionStatus[dbId].connection = new pg.Client(conString);
+
+        connectionStatus[dbId].connection.connect(function(err) {
+            done(err);
+        });
+    }
+
+    else {
+        handle["showError"](response, "The database with id '" +  dbId + "' is absent in the configuration");
     }
 }
 
@@ -91,16 +107,14 @@ function dataInput(err, dbId, handle, handleKey, response, pathname) {
     var table_groups = '';
 
     if (err) {
-        connectionStatus.baseId = '';
-        connectionStatus.status = false;
+        connectionStatus[dbId].status = false;
         handle["showError"](response, "Error connecting to the database with id '" + dbId + "'. " + err);
     } else {
-        connectionStatus.baseId = dbId;
-        connectionStatus.status = true;
+        connectionStatus[dbId].status = true;
         if (config.db[dbId].table_groups) {
             table_groups = config.db[dbId].table_groups;
         }
-        handle[handleKey](response, connectionStatus.connection, pathname, config.db[dbId].type, table_groups);
+        handle[handleKey](response, connectionStatus[dbId].connection, pathname, config.db[dbId].type, table_groups);
     }
 }
 
